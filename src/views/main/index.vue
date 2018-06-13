4d18701414701e769d85fff539bce629f11c2037
wenxuesay <36788475+wenxuesay@users.noreply.github.com>
2018-06-13 19:23:20 +0800

    Create index.vue

---
+++ src/views/main/index.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  article(style="flex:1;background: url('/static/img/2.jpg')")
+    //- img(src="/static/img/2.jpg")
+</template>
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+img {
+  height: 100%;
+}
+</style>
+
