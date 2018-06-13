6dd9b05cf34bc528a5f272838be1dd837f14ca2b
wenxuesay <36788475+wenxuesay@users.noreply.github.com>
2018-06-13 18:05:08 +0800

    Create layout.vue

---
+++ src/views/access/koa/common/layout.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+bdp-container
+  bdp-silder
+    bdp-menu(:menus="menus")
+  bdp-main
+    router-view
+</template>
+<script>
+import { menu } from '../../router/common';
+
+export default {
+  data() {
+    return {
+      menus: menu,
+    };
+  },
+};
+</script>
+<style lang="scss">
+
+</style>
