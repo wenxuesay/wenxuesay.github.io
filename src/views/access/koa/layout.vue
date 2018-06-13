cc20ceacd7c93bbbb80450106afe7be5b944b610
wenxuesay <36788475+wenxuesay@users.noreply.github.com>
2018-06-13 18:00:02 +0800

    Create layout.vue

---
+++ src/views/access/koa/layout.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+bdp-container
+  bdp-silder
+    bdp-menu(:menus="menus")
+  bdp-main
+    router-view
+</template>
+<script>
+import { menu } from '../../router/access';
+
+export default {
+  data() {
+    return {
+      menus: menu,
+    };
+  },
+  mounted() {
+    const $vm = this;
+    this.menus = this.menus.map((v) => {
+      const obj = Object.assign({}, v);
+      if (obj.meta) {
+        obj.meta.show = $vm.$route.path.toString().includes(v.path);
+      }
+      return obj;
+    });
+  },
+};
+</script>
+<style lang="scss">
+
+</style>
