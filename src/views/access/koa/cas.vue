0c9ee3c1a95ae3d59ff1912eb4c32adb3f29c648
wenxuesay <36788475+wenxuesay@users.noreply.github.com>
2018-06-13 17:46:04 +0800

    Create cas.vue

---
+++ src/views/access/koa/cas.vue
@@ -0,0 +1,22 @@
+<template>
+  <article class="markdown">
+    <p class="t22">
+      cas库(配置文件请参考环境配置)
+    </p>
+    <p class="t18">
+      由于npm上的源都通不过的CAS,我们自己写了一套拦截逻辑,可参考自行配置。
+    </p>
+    <pre v-highlightjs><code>
+class cas {
+  constructor ({casUrl, serverUrl, indexUrl, serviceValidate = '/cas/serviceValidate', loginUrl = '/cas/login', logoutUrl = '/cas/logout'} = {}) {
+    this.casUrl = casUrl
+    this.serverUrl = serverUrl
+    this.serviceValidate = serviceValidate
+    this.indexUrl = indexUrl
+    this.loginUrl = loginUrl
+    this.logoutUrl = logoutUrl
+  }
+}
+    </code></pre>
+  </article>
+</template>
