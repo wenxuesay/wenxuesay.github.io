61fbdf365c90d1b512c5374468d6790c8d0173aa
wenxuesay <36788475+wenxuesay@users.noreply.github.com>
2018-06-13 18:01:06 +0800

    Create start.vue

---
+++ src/views/access/koa/start.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+article.markdown
+  p.t22 接入层
+  p.t18 前言
+  p.t16 对于现有业务,数据通过业务逻辑筛选获取有用的数据,存入mysql,ElaticSearch,Hive。前端可以通过接入层获取VO数据,结合页面逻辑作为数据展示,可以减少前后联调时间，增加工作效率。
+  p.t16 为此我们提供了两套web后台框架Koa(nodejs),SpringBoot(Java)。
+  p.t16
+    | git地址:
+  p.t16
+    a(href="http:///repos/koa2-api/browse") koa2
+  p.t16
+    a(href="http:///repos/springboot/browse") springboot
+</article>
+</template>
