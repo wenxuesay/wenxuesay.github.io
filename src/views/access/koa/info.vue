dc9e522368dfc9bd66d9ae2ef8c15f2c4c88a799
wenxuesay <36788475+wenxuesay@users.noreply.github.com>
2018-06-13 17:56:47 +0800

    Create info.vue

---
+++ src/views/access/koa/info.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+  article.markdown
+    .t22
+      a(href="https://koa.bootcss.com/") Koa -- 基于 Node.js 平台的下一代 web 开发框架
+    .t18 koa 是由 Express 原班人马打造的，致力于成为一个更小、更富有表现力、更健壮的 Web 框架。 使用 koa 编写 web 应用，通过组合不同的 generator，可以免除重复繁琐的回调函数嵌套， 并极大地提升错误处理的效率。koa 不在内核方法中绑定任何中间件， 它仅仅提供了一个轻量优雅的函数库，使得编写 Web 应用变得得心应手。
+    .t18 对于现用业务,数据业务推送到mysql,或者推送到elasticsearch,我们直接通过node.js返回数据到前台。
+</template>
