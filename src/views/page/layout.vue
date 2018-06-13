24e1a5109598359f394d5347e4916d1d73472a12
wenxuesay <36788475+wenxuesay@users.noreply.github.com>
2018-06-13 19:25:22 +0800

    Create layout.vue

---
+++ src/views/page/layout.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+bdp-container
+  bdp-silder
+    bdp-menu(:menus="menus")
+  bdp-main
+    router-view
+</template>
+<script>
+import { menu } from '../../router/page';
+
+export default {
+  data() {
+    return {
+      menus: menu,
+    };
+  },
+};
+</script>
